0f2f5047000a2534ac5d81436dbc6d89b17c97ad
Onik <90210498+Onik11@users.noreply.github.com>
2023-07-21 16:44:58 +0400

    aa
    aa

---
+++ incdecthreads-skeleton/src/main/java/com/epam/rd/autotask/thread/IncDecThreads.java
@@ -0,0 +1,29 @@
+package com.epam.rd.autotask.thread;
+
+public class IncDecThreads {
+
+    static final int COUNT = 5000;
+    static long value;
+
+    /**
+     * In a loop increments {@code COUNT} times the {@code value}
+     * and prints to the console the name of the class, the name of
+     * the thread and the value of the field {@code value}.
+     */
+    static class Increment {
+        // TODO place your code here
+    }
+
+    /**
+     * In a loop decrements {@code COUNT} times the {@code value}
+     * and prints to the console the name of the class, the name of
+     * the thread and the value of the field {@code value}.
+     */
+    static class Decrement {
+        // TODO place your code here
+    }
+
+    public static void main(String[] args) {
+        // TODO place your code here
+    }
+}
